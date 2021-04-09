ff5d4738ef040f54217687b7076800ce830c914e
Andy Woodward <andyw23@gmail.com>
2021-04-09 13:07:36 -0500

    Update tp 1.0.2 since 1.0.1 didn't catch the Hash.cs file.
    * Added Hash class to hash data.
    * Added Release Notes.md file

---
+++ Hash.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace Utility.Encryption
+{
+    public class Hash
+    {
+        /// <summary>
+        /// Keeping this is an an example and for backwards compatibility with another app
+        /// </summary>
+        /// <param name="input">Date you want hashed</param>
+        /// <returns></returns>
+        public static string CalculateMD5Hash(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                return GetHash(md5, input);
+            }
+        }
+
+        public static string GetHash(System.Security.Cryptography.HashAlgorithm hashAlgorithm, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        public static bool VerifyHash(System.Security.Cryptography.HashAlgorithm hashAlgorithm, string input, string hash)
+        {
+            // Hash the input.
+            var hashOfInput = GetHash(hashAlgorithm, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
+        }
+
+    }
+}
---
+++ Release Notes.md
@@ -0,0 +1,15 @@
+# Utility.Encryption
+***
+A Utility that allows you to encrypt data using Rijndael/AES and validate/hash signatures for signing data coming from or sending to a 3rd party.
+
+***
+**Update 1.0.2 - 04/09/2021**
+* Added Hash class to hash data.
+* Added Release Notes.md file
+
+**Update 1.0.1 - 04/09/2021**
+* Added SignatureValidation class to validate siging data.
+* Added readme.md file
+
+**Update 1.0 - 04/06/2021**
+* Initial Release
