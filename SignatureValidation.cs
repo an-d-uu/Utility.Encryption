c290a57ade0a6c1c3ef5d84089137b09816a47af
Andy Woodward <andyw23@gmail.com>
2021-04-09 12:48:53 -0500

    Updated to 11.0.1
    * Added SignatureValidation class to validate siging data.

---
+++ SignatureValidation.cs
@@ -0,0 +1,280 @@
+﻿
+using System;
+using System.Security.Cryptography;
+
+namespace Utility.Encryption
+{
+#if NET40
+    public enum eCryptographyType
+    {
+        HMACMD5,
+        HMACRIPEMD160,
+        HMACSHA1,
+        HMACSHA256,
+        HMACSHA384
+
+    }
+#elif NET5_0_OR_GREATER
+public enum eCryptographyType
+    {
+        HMACMD5,
+        HMACSHA1,
+        HMACSHA256,
+        HMACSHA384,
+        HMACSHA512
+    }
+#else
+public enum eCryptographyType
+    {
+        HMACMD5,
+        HMACRIPEMD160,
+        HMACSHA1,
+        HMACSHA256,
+        HMACSHA384,
+        HMACSHA512
+    }
+#endif
+
+    public class SignatureValidation
+    {
+        /// <summary>
+        /// Adds a useful epoch datetime function for including in hashing and validating signatures
+        /// </summary>
+        /// <param name="unixTime"></param>
+        /// <returns></returns>
+        public static DateTime FromUnixTime(long unixTime)
+        {
+            return epoch.AddMilliseconds(unixTime);
+        }
+        private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+
+        /// <summary>
+        /// Validates the signature.
+        /// </summary>
+        /// <returns>true or false</returns>
+        public static bool validSignature(string data, string secret, eCryptographyType cryptography)
+        {
+            bool returnValue = false;
+
+            try
+            {
+                string hashedDataSig = hashData(data, secret, cryptography);
+
+                if (hashedDataSig == secret)
+                {
+                    returnValue = true;
+                }
+                else
+                {
+                    returnValue = false;
+                }
+            }
+            catch
+            {
+                returnValue = false;
+            }
+
+            return returnValue;
+        }
+
+#if NET40
+        public static string hashData(string data, string secret, eCryptographyType cryptography)
+        {
+            string returnValue = string.Empty;
+            secret = secret ?? "";
+            var encoding = new System.Text.UTF8Encoding();
+            byte[] keyByte = encoding.GetBytes(secret);
+            byte[] messageBytes = encoding.GetBytes(data);
+
+            switch (cryptography)
+            {
+                case eCryptographyType.HMACRIPEMD160:
+                    {
+
+                        using (HMACRIPEMD160 hashfunction = new HMACRIPEMD160(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACMD5:
+                    {
+
+                        using (HMACMD5 hashfunction = new HMACMD5(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA1:
+                    {
+
+                        using (HMACSHA1 hashfunction = new HMACSHA1(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA256:
+                    {
+                        using (HMACSHA256 hashfunction = new HMACSHA256(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA384:
+                    {
+                        using (HMACSHA384 hashfunction = new HMACSHA384(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+            }
+
+            return returnValue;
+        }
+#elif NET5_0_OR_GREATER
+        public static string hashData(string data, string secret, eCryptographyType cryptography)
+        {
+            string returnValue = string.Empty;
+            secret = secret ?? "";
+            var encoding = new System.Text.UTF8Encoding();
+            byte[] keyByte = encoding.GetBytes(secret);
+            byte[] messageBytes = encoding.GetBytes(data);
+
+            switch (cryptography)
+            {
+                case eCryptographyType.HMACMD5:
+                    {
+
+                        using (HMACMD5 hashfunction = new HMACMD5(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA1:
+                    {
+
+                        using (HMACSHA1 hashfunction = new HMACSHA1(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA256:
+                    {
+                        using (HMACSHA256 hashfunction = new HMACSHA256(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA384:
+                    {
+                        using (HMACSHA384 hashfunction = new HMACSHA384(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA512:
+                    {
+                        using (HMACSHA512 hashfunction = new HMACSHA512(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+            }
+
+            return returnValue;
+        }
+#else
+        public static string hashData(string data, string secret, eCryptographyType cryptography)
+        {
+            string returnValue = string.Empty;
+            secret = secret ?? "";
+            var encoding = new System.Text.UTF8Encoding();
+            byte[] keyByte = encoding.GetBytes(secret);
+            byte[] messageBytes = encoding.GetBytes(data);
+
+            switch (cryptography)
+            {
+                case eCryptographyType.HMACRIPEMD160:
+                    {
+
+                        using (HMACRIPEMD160 hashfunction = new HMACRIPEMD160(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACMD5:
+                    {
+
+                        using (HMACMD5 hashfunction = new HMACMD5(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA1:
+                    {
+
+                        using (HMACSHA1 hashfunction = new HMACSHA1(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA256:
+                    {
+                        using (HMACSHA256 hashfunction = new HMACSHA256(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA384:
+                    {
+                        using (HMACSHA384 hashfunction = new HMACSHA384(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+                case eCryptographyType.HMACSHA512:
+                    {
+                        using (HMACSHA512 hashfunction = new HMACSHA512(keyByte))
+                        {
+                            byte[] hashmessage = hashfunction.ComputeHash(messageBytes);
+                            returnValue = Convert.ToBase64String(hashmessage);
+                        }
+                        break;
+                    }
+            }
+
+            return returnValue;
+        }
+#endif
+    }
+}
--- readme.md
+++ readme.md
@@ -1,8 +1,27 @@
 # Utility.Encryption
 ***
+A Utility that allows you to encrypt data using Rijndael/AES and validate/hash signatures for signing data coming from or sending to a 3rd party.
+
+
+### Available Classes
+
+#### RijndaelEnhanced
+***
 This class uses a symmetric key algorithm (Rijndael/AES) to encrypt and
 decrypt data. As long as it is initialized with the same constructor
 parameters, the class will use the same key. Before performing encryption,
 the class can prepend random bytes to plain text and generate different
 encrypted values from the same plain text, encryption key, initialization
 vector, and other parameters. This class is thread-safe.
+
+#### SignatureValidation
+***
+This class allows users to use the following hashing algorithms to hash data and verify hashed data.
+* HMACMD5
+* HMACRIPEMD160
+  * unavailable in .NET 5 and greater
+* HMACSHA1
+* HMACSHA256
+* HMACSHA384
+* HMACSHA512
+  * unavailable in .NET 4.0
